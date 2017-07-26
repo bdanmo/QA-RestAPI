c56d2463522847f42040f21f5da6bcec7ea74a4c
Brendan Moran <b.daniel.moran@gmail.com>
2017-07-26 16:59:09 -0400

    Initialized express app.

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-'use strict'
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000; //eval to 3000 unless deploying to production env
+
+app.listen(port, () => console.log('Express server listening on port', port));
